184ad5d157f35b87eb8703298faf1410437cf86b
Klaudiusz Staniek <kstaniek@gmail.com>
2016-03-09 22:46:57 +0100

    doc update

--- condoor/__init__.py
+++ condoor/__init__.py
@@ -619,11 +619,12 @@ class Connection(object):
     def udi(self):
         """Returns the dict representing the udi hardware record::
 
-            {'description': 'ASR-9904 AC Chassis',
-            'name': 'Rack 0',
-            'pid': 'ASR-9904-AC',
-            'sn': 'FOX1830GT5W ',
-            'vid': 'V01'
+            {
+             'description': 'ASR-9904 AC Chassis',
+             'name': 'Rack 0',
+             'pid': 'ASR-9904-AC',
+             'sn': 'FOX1830GT5W ',
+             'vid': 'V01'
             }
 
         """
@@ -633,11 +634,13 @@ class Connection(object):
     def device_info(self):
         """Returns the dict represeing the device info record::
 
-            {'family': 'ASR9K',
-            'os_type': 'eXR',
-            'os_version': '6.1.0.06I',
-            'platform': 'generic'
+            {
+             'family': 'ASR9K',
+             'os_type': 'eXR',
+             'os_version': '6.1.0.06I',
+             'platform': 'generic'
             }
+            
         """
         _device_info = {
             'family': self._family,
